209b0d28d14deb3f34ee462394fe64f69d01d67f
Hugo Gonza <hugoteg10@gmail.com>
2025-09-29 19:06:10 +0200

    Agregadas nuevas funciones al main

---
+++ lib/rectangulo.py
@@ -0,0 +1,8 @@
+def get_identificador():
+    return "Rectangulo"
+
+def get_area(base:int , altura: int)-> int:
+    return base*altura
+
+def get_perimetro(base:int , altura : int):
+    return base*2 + altura*2
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+print("---------------------------")
 from lib import cuadrado
-print("Proyecto Figuras")
+print("Proyecto Figuras INSO")
 lado = 4
 print(f"El area del cuadrado de lado {lado} es {cuadrado.get_area(lado)}")
+
+print("---------------------------")
