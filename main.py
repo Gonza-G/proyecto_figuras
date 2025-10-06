dcde892c6374862a6a33b47768cc745a8d9ffdfb
Hugo Gonza <hugoteg10@gmail.com>
2025-10-06 18:34:48 +0200

    Agregadas funcionalidades para el tirangulo

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-print("---------------------------")
-from lib import cuadrado
-print("Proyecto Figuras INSO")
-lado = 4
-print(f"El area del cuadrado de lado {lado} es {cuadrado.get_area(lado)}")
+from lib import cuadrado, triangulo
+print("Proyecto Figuras")
+print(cuadrado.get_identificador())
+lado=4
+print(f"El área de un {cuadrado.get_identificador()} de lado {lado} es: {cuadrado.get_area(lado)} y el perímetro es {cuadrado.get_perimetro(lado)}")
 
-print("---------------------------")
+base=4
+altura=2
+print(triangulo.get_identificador())
+print(f"El área de un {triangulo.get_identificador()} de base {base} y altura {altura} es: {triangulo.get_area(base,altura)} y el perímetro es {triangulo.get_perimetro(base,base,base)}")
